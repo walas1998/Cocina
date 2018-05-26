3d691fb97fc7ec969847de8401475080427ec19e
walas1998 <willi_gta@hotmail.com>
2018-05-26 11:04:53 -0500

    Add files via upload

---
+++ Cocina/WilliamGalvez/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Que desea cocinar");
+            Console.ReadLine();
+
+
+            Console.WriteLine("Nombre lo que nesesita");
+            Console.ReadLine();
+            //int numeroMin = int.Parse (Console.ReadLine());
+
+            /*Console.WriteLine("Utencilios");
+            Console.ReadLine();
+
+            Console.WriteLine("Herramientas");
+            Console.ReadLine();
+
+            Console.WriteLine("Electrodomesticos Nesesarios");
+            Console.ReadLine();*/
+
+
+
+        }
+    }
+}
